4a41362b8655448a48de575ba6a1a17daca69a8d
Lawrence Mile <Laurancemile@gmail.com>
2024-10-13 14:22:33 +0200

    Dom content after function

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-document.addEventListener("DOMContentLoaded", () => {
-	const addBtnEl = document.getElementById("add-task-btn");
-	const taskInputEl = document.getElementById("task-input");
-	const taskListEl = document.getElementById("task-list");
-	const todoAppEl = document.getElementById("todo-app");
+const addBtnEl = document.getElementById("add-task-btn");
+const taskInputEl = document.getElementById("task-input");
+const taskListEl = document.getElementById("task-list");
+const todoAppEl = document.getElementById("todo-app");
 
-	const addTask = () => {
-		// add new task to the list
-		const newItem = document.createElement("li");
-		const inputValue = taskInputEl.value.trim();
+function addTask() {
+	// add new task to the list
+	const newItem = document.createElement("li");
+	const inputValue = taskInputEl.value.trim();
 
-		if (inputValue) {
-			newItem.innerHTML = `
+	if (inputValue) {
+		newItem.innerHTML = `
 				${taskInputEl.value} <button class='remove-btn'>Remove</button>
 			`;
-			taskListEl.appendChild(newItem);
-			taskInputEl.value = "";
-		} else {
-			alert("Enter a valid input!");
-		}
-	};
+		taskListEl.appendChild(newItem);
+		taskInputEl.value = "";
+	} else {
+		alert("Enter a valid input!");
+	}
+}
 
-	const removeTask = (e) => {
-		// remove the list item clicked
-		if (e.target.classList.contains("remove-btn")) {
-			const listItem = e.target.parentElement;
-			listItem.remove();
-		}
-	};
+function removeTask(e) {
+	// remove the list item clicked
+	if (e.target.classList.contains("remove-btn")) {
+		const listItem = e.target.parentElement;
+		listItem.remove();
+	}
+}
 
-	todoAppEl.addEventListener("click", removeTask);
-	addBtnEl.addEventListener("click", addTask);
-	document.addEventListener("keypress", (e) => {
-		// add item using the key enter button
-		if (e.key === "Enter") {
-			addTask();
-		}
-	});
+todoAppEl.addEventListener("click", removeTask);
+addBtnEl.addEventListener("click", addTask);
+document.addEventListener("keypress", (e) => {
+	// add item using the key enter button
+	if (e.key === "Enter") {
+		addTask();
+	}
 });
+
+document.addEventListener("DOMContentLoaded", () => {});
